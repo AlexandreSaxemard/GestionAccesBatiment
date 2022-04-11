a250d5f00bd55b3ef3b5bedbdb250079b340e6ea
Alexandre Saxemard <alexandre.saxemard@etu.univ-cotedazur.fr>
2022-04-11 15:26:35 +0200

    Zone B Arduino/M5Stack à tester

---
+++ Zone B/ZoneB_RFID_Arduino/ZoneB_RFID_Verif.ino
@@ -0,0 +1,51 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+// Entrées des pins du RC522
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);   
+
+void setup(){
+  SPI.begin();     
+  rfid.PCD_Init();   
+  Serial.begin(9600);
+  Serial.println("Scan RFID Card");
+}
+
+void loop(){
+  if ( ! rfid.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+
+  if ( ! rfid.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  Serial.println("UID tag :");
+  
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < rfid.uid.size; i++){
+    Serial.println(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.println(rfid.uid.uidByte[i], HEX);
+    content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(rfid.uid.uidByte[i], HEX));
+  }
+  Serial.println("Message : ");
+  
+  content.toUpperCase();
+  if (content.substring(1) == "59 64 CC 48"){ //Plz change to your cards UID
+    Serial.println("Autorisé !");
+    delay(3000);
+    setup();
+  }
+ 
+  else{
+    Serial.println("Accès refusé !");
+    delay(3000);
+    setup();
+  }
+} 
