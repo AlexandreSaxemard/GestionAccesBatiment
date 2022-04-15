2cd5fcce9a882b0155b984bd9faa580497929797
Alexandre Saxemard <alexandre.saxemard@etu.univ-cotedazur.fr>
2022-04-15 16:17:25 +0200

    Code de la zone B

--- Zone B/ZoneB_RFID_Arduino_MQTT/ZoneB_RFID_Arduino_MQTT.ino
+++ Zone B/ZoneB_RFID_Arduino_MQTT/ZoneB_RFID_Arduino_MQTT.ino
@@ -1,7 +1,7 @@
-#include <M5Stack.h>
+#include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <WiFi.h>
+#include <WiFiClient.h>
 #include "PubSubClient.h"
 
 // Configuration du réseau WIFI
@@ -10,9 +10,8 @@ const char* password = "0918273645";
 
 static const char* connectionString = "";
 
-//const char* mqtt_server = "192.168.43.XX";
 const char* mqtt_server = "172.24.102.120";
-const char* badge_topic = "Arduino/ZoneB";
+const char* badge_topic = "Arduino/Test2";
 
 const char* clientID = "Arduino_ZoneB";
 
@@ -20,16 +19,15 @@ WiFiClient wifiClient;
 PubSubClient client(mqtt_server, 1883, wifiClient);
 
 // Entrées des pins du RC522
-#define SS_PIN 21
-#define RST_PIN 33
+#define SS_PIN D8
+#define RST_PIN D3
 
-MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN); 
 
 // Initialisation de la variable où l'UID du badge sera stocké
 byte nuidPICC[4];
 
 void setup(){
-  M5.begin();
   Serial.begin(115200);
   Serial.println("Tentative de connexion au WiFi...");
 
@@ -54,18 +52,6 @@ void setup(){
   SPI.begin(); 
   rfid.PCD_Init(); 
   Serial.println("Scan de tag RFID...");
-  M5.Lcd.setCursor(0, 20);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.println("En attente d'un badge...");
-}
-
-void loop(){
-  Serial.println("Envoi de l'UID detecté...");
-  readRFID();
-  delay(200);
-  if(client.publish(badge_topic, (byte*) &nuidPICC, sizeof(nuidPICC))){
-    Serial.println("UID du badge envoyé!");
-  }
 }
 
 void readRFID(){
@@ -82,7 +68,6 @@ void readRFID(){
   rfid.uid.uidByte[2] != nuidPICC[2] ||
   rfid.uid.uidByte[3] != nuidPICC[3] ){
   Serial.println("Une nouvelle carte a bien été detectée !");
-  //M5.Lcd.println("Une nouvelle carte a été detectée !");
   
   // On stocke l'UID du badge dans le tableau
   for (byte i = 0; i < 4; i++){
@@ -90,23 +75,36 @@ void readRFID(){
   }
   
   Serial.print("UID du badge: ");
-  //M5.Lcd.print("UID du badge: ");
+  for (byte i = 0; i < 4; i++) 
+  {
+    //Serial.print(nuidPICC[i], HEX);
+    Serial.print(" ");
+  }
   printDec(rfid.uid.uidByte, rfid.uid.size);
   Serial.println();
-  //M5.Lcd.println();
   }
-  
+
+  client.publish(badge_topic, (byte*) &nuidPICC, sizeof(nuidPICC));
+  Serial.println("UID du badge envoyé!");
+ 
   // Arrêter le PICC
   rfid.PICC_HaltA();
   // On stop le cryptage sur PCD
   rfid.PCD_StopCrypto1();
 }
 
-void printDec(byte *buffer, byte bufferSize){
-  for (byte i = 0; i < bufferSize; i++){
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    //M5.Lcd.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], DEC);
-    //M5.Lcd.print(buffer[i], DEC);
-  }
+
+void loop() 
+{ 
+  readRFID();
+  /*if(client.publish(badge_topic, (byte*) &nuidPICC, sizeof(nuidPICC))){
+    Serial.println("UID du badge envoyé!");
+  }*/
+}
+
+void printDec(byte *buffer, byte bufferSize) {
+ for (byte i = 0; i < bufferSize; i++) {
+   Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+   Serial.print(buffer[i], DEC);
+ }
 }
