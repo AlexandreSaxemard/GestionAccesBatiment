7fe1a0e2af8e815b7ca507dc1b7bf138144fb637
Alexandre Saxemard <alexandre.saxemard@etu.univ-cotedazur.fr>
2022-05-05 09:36:40 +0200

    GoogleAgendaAPI Delete

--- GoogleOAuth/sessionVerif.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_SESSION['start']) && (time() - $_SESSION['start'] > 10000)) {
-    session_unset(); 
-    session_destroy();
-    header('Location: index.php');
-}
-$_SESSION['start'] = time();
-?>
